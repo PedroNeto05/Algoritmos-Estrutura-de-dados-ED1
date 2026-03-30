26247d3e796742a11850b213b0b9b88dfebf3e70
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2026-03-29 22:04:22 -0300

    inicio da implementacao linkedlist

---
+++ list/linkedlist.go
@@ -0,0 +1,90 @@
+package list
+
+import "errors"
+
+type Node struct {
+	val  int
+	next *Node
+}
+
+type LinkedList struct {
+	head     *Node
+	tail     *Node
+	inserted int
+}
+
+func (l *LinkedList) Init() {
+}
+
+func (l *LinkedList) Add(val int) {
+	newNode := &Node{
+		val:  val,
+		next: nil,
+	}
+	if l.inserted == 0 {
+		l.head = newNode
+		l.tail = l.head
+		return
+	}
+
+	currNode := l.head
+
+	for currNode.next != nil {
+		currNode = currNode.next
+	}
+
+	currNode.next = newNode
+	l.tail = newNode
+
+	l.inserted++
+}
+
+// func (l *LinkedList) AddOnIndex(val, index int) error {}
+
+func (l *LinkedList) Get(index int) (int, error) {
+	if l.inserted == 0 {
+		return -1, errors.New("o vetor esta vazio")
+	}
+	if index >= l.inserted || index < 0 {
+		return -1, errors.New("index fora do vetor")
+	}
+
+	count := 0
+	currNode := l.head
+
+	for currNode.next != nil {
+		if count == index {
+			break
+		}
+		currNode = currNode.next
+		count++
+	}
+	return currNode.val, nil
+}
+
+func (l *LinkedList) Set(val, index int) error {
+	if l.inserted == 0 {
+		return errors.New("o vetor esta vazio")
+	}
+	if index >= l.inserted || index < 0 {
+		return errors.New("index fora do vetor")
+	}
+
+	count := 0
+	currNode := l.head
+
+	for currNode.next != nil {
+		if count == index {
+			break
+		}
+		currNode = currNode.next
+		count++
+	}
+	currNode.val = val
+	return nil
+}
+
+// func (l *LinkedList) RemoveOnIndex(index int) error {}
+func (l *LinkedList) Size() int {
+	return l.inserted
+}
