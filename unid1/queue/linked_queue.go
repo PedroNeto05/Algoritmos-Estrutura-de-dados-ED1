6aa3e0aa815f10eff8db7f1e4a038c0bfa4cc030
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2026-04-20 17:25:50 -0300

    mudando metodo

--- unid1/queue/linked_queue.go
+++ unid1/queue/linked_queue.go
@@ -13,49 +13,48 @@ type LinkedQueue struct {
 	size  int
 }
 
-func (l *LinkedQueue) Enqueue(val int) {
+func (q *LinkedQueue) Enqueue(val int) {
 	node := &Node{
 		val:  val,
 		next: nil,
 	}
 
-	if l.size == 0 {
-		l.front = node
-		l.back = node
-		l.size++
+	if q.size == 0 {
+		q.front = node
+		q.back = node
+		q.size++
 		return
 	}
 
-	l.back.next = node
-	l.back = node
-	l.size++
+	q.back.next = node
+	q.back = node
+	q.size++
 }
 
-func (l *LinkedQueue) Dequeue() (int, error) {
-	if l.size < 1 {
+func (q *LinkedQueue) Dequeue() (int, error) {
+	if q.size < 1 {
 		return -1, errors.New("lista vazia")
 	}
-	val := l.front.val
-	l.front = l.front.next
-	l.size--
-	if l.size == 0 {
-		l.back = nil
+	val := q.front.val
+	q.front = q.front.next
+	q.size--
+	if q.size == 0 {
+		q.back = nil
 	}
 	return val, nil
 }
 
-func (l *LinkedQueue) Front() (int, error) {
-	if l.size < 1 {
+func (q *LinkedQueue) Front() (int, error) {
+	if q.size < 1 {
 		return -1, errors.New("lista vazia")
 	}
-	return l.front.val, nil
+	return q.front.val, nil
 }
 
-func (l *LinkedQueue) IsEmpty() bool {
-	return l.size == 0
+func (q *LinkedQueue) IsEmpty() bool {
+	return q.size == 0
 }
 
-func (l *LinkedQueue) Size() int {
-	return l.size
+func (q *LinkedQueue) Size() int {
+	return q.size
 }
-
